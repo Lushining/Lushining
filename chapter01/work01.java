be65943cd0032f54f2f4fd1464e91f8f2ba88460
Lushining <1448239958@qq.com>
2023-02-08 17:16:45 +0800

    delete and add chapter07

--- chapter01/work01.java
+++
@@ -1,6 +0,0 @@
-//??§Õhello,world????
-public class work01{
-    public static void main(String[] args){
-        System.out.println("hello,world");
-    }
-}
--- chapter01/work02.java
+++
@@ -1,6 +0,0 @@
-//分行打印（姓名、性别、籍贯、住址）
-public class work02{
-    public static void main(String[] args){
-        System.out.println("姓名\t性别\t籍贯\t住址\n卢诗宁\t女\t浙江\t绍兴");
-    }
-}
